8f0db8dc4d7bfd833f2102920605e6f26485d5b5
Trey Zima <trey.m.zima@gmail.com>
2024-11-16 15:29:40 -0700

    Completed Step 1

--- hoisting.js
+++ hoisting.js
@@ -1,6 +1,9 @@
 /* Task 1: Declare a Destination Variable */
 // TODO: Use `let` to declare a variable named `destination` and assign it the value `"Ancient Egypt"`. Print the destination to the console.
 
+let destination = "Ancient Egypt";
+console.log("Destination:", destination);
+
 /* Task 2: Change the Destination */
 // TODO: Now, change the `destination` variable to `"Medieval Europe"`. Print the new destination to the console.
 
